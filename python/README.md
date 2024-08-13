6f246db8bd2fba9460132276a587521721c496d8
sanimkhan <sanimkhan13@gmail.com>
2024-08-13 21:22:43 +0600

    Python- data types

---
+++ python/README.md
@@ -0,0 +1,4 @@
+# Data types
+
+## Blog post
+Detailed blog post [How I choose between Python Arrays, Lists, Tuples, Sets, and Dictionaries](https://blog.devops.dev/how-i-choose-between-python-arrays-lists-tuples-sets-and-dictionaries-3f08a7c4513f).
---
+++ python/data_types.py
@@ -0,0 +1,28 @@
+# Arrays
+import array as arr
+my_array = arr.array('i', [1, 2, 3, 4])
+my_array.append(5)
+print(my_array)  # Output: array('i', [1, 2, 3, 4, 5])
+
+# Lists
+my_list = [1, "two", 3, "four"]
+my_list.append("five")
+print(my_list)  # Output: [1, "two", 3, "four", "five"]
+
+# Tuples
+my_tuple = (1, "two", 3, "four")
+print(my_tuple)  # Output: (1, "two", 3, "four")
+
+
+# Sets
+my_set = {1, "two", 3, "four"}
+my_set.add("five")
+print(my_set)  # Output: {1, 3, "two", "four", "five"}
+
+# Dictionaries
+my_dict = {"one": 1, "two": 2, "three": 3}
+my_dict["four"] = 4
+print(my_dict)  # Output: {"one": 1, "two": 2, "three": 3, "four": 4
+
+
+
